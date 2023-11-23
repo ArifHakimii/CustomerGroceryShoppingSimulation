14f0deac5c903f3d4de9278e088674eedc5c1216
ArifHakimii <89134407+ArifHakimii@users.noreply.github.com>
2023-11-24 02:56:46 +0800

    Update AgentQueue.cs

--- Assets/AgentQueue.cs
+++ Assets/AgentQueue.cs
@@ -1,3 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stall : MonoBehaviour
+{
+    // Variable to store the attractiveness of the stall
+    private float attractiveness;
+
+    // Start method is called before the first frame update
+    void Start()
+    {
+        // Generate a random attractiveness value between 0 and 100
+        attractiveness = Random.Range(0, 100);
+    }
+
+    // Update method is called once per frame, currently empty
+    void Update()
+    {
+        // This method is currently empty, but it can be used to implement behavior
+        // related to the stall's attractiveness, such as attracting customers or
+        // adjusting the attractiveness based on customer interactions.
+    }
+}
+
 public class AgentQueue : MonoBehaviour
 {
     // Reference to the queue list manager
