6b57f28e8f38c0a03224cd656c46871971582ac5
ArifHakimii <89134407+ArifHakimii@users.noreply.github.com>
2023-11-24 02:46:27 +0800

    Update AgentQueue.cs

--- Assets/AgentQueue.cs
+++ Assets/AgentQueue.cs
@@ -1,79 +1,132 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class AgentQueue : MonoBehaviour
 {
+    // Reference to the queue list manager
     public QueueList queueList;
 
+    // List of agents currently in the queue
     List<Agent> agentqueue = new List<Agent>();
 
-    //string counterId;
-    Vector3 qdir;  // queue direction
-    Vector3 qPos;
-    Agent incomingAgent;
-    float queueSpacing = 1;
-  
+    // Vector3 representing the direction of the queue
+    Vector3 qdir;
 
-    // Start is called before the first frame update
+    // Vector3 representing the position of the queue
+    Vector3 qPos;
+
+    // Reference to the agent currently entering the queue
+    Agent incomingAgent;
+
+    // Spacing between agents in the queue
+    float queueSpacing = 1;
+
+    // Start method is called before the first frame update
     void Start()
     {
-        qdir = -transform.right; // the red vector
-        qPos = transform.position + qdir*(agentqueue.Count+1)*queueSpacing;
+        // Set the queue direction to the negative right vector (red vector)
+        qdir = -transform.right;
+
+        // Calculate the initial position of the queue based on the queue direction and spacing
+        qPos = transform.position + qdir * (agentqueue.Count + 1) * queueSpacing;
+
+        // Add this queue to the queue list manager
         queueList.Add(this);
 
-        Debug.Log("Queue created for "+gameObject.name+". With pos: "+qPos);
-
+        // Log the creation of the queue
+        Debug.Log("Queue created for " + gameObject.name + ". With pos: " + qPos);
     }
 
-    // Append a new agent coming into the queue and positioning the agent in line for the queue
-    public bool Add(Agent agent) {
-        if (incomingAgent != null) {
-            return false;
+    // Add a new agent to the queue and position it accordingly
+    public bool Add(Agent agent)
+    {
+        // Check if an agent is already in the process of entering the queue
+        if (incomingAgent != null)
+        {
+            return false; // Fail to add the agent
         }
+
+        // Set the incoming agent to the specified agent
         incomingAgent = agent;
+
+        // Set the y-coordinate of the queue position to match the agent's y-coordinate
         qPos.y = agent.GetPos().y;
+
+        // Set the agent's destination to the queue position
         agent.SetDestination(qPos);
-        queueSpacing = agent.GetRadius()*2f;
-        qPos += qdir*queueSpacing;
-        return true;        
+
+        // Adjust the queue spacing based on the agent's radius
+        queueSpacing = agent.GetRadius() * 2f;
+
+        // Update the queue position to accommodate the new agent
+        qPos += qdir * queueSpacing;
+
+        // Successfully added the agent to the queue
+        return true;
     }
 
-    // Add space between agents in the queue
-    public void Shift() {
-        foreach (Agent agent in agentqueue) {
-            agent.transform.position -= qdir*queueSpacing;
+    // Shift the positions of all agents in the queue to maintain spacing
+    public void Shift()
+    {
+        // Iterate through each agent in the queue
+        foreach (Agent agent in agentqueue)
+        {
+            // Move the agent's position back by the queue spacing
+            agent.transform.position -= qdir * queueSpacing;
         }
-        qPos -= qdir*queueSpacing; 
+
+        // Update the queue position to reflect the shift
+        qPos -= qdir * queueSpacing;
     }
 
-    // Remove agent in position 0 from queue
-    public Agent Pop() {
+    // Remove the first agent from the queue
+    public Agent Pop()
+    {
+        // Get the first agent in the queue
         Agent agent = agentqueue[0];
-        agentqueue.RemoveAt(0); 
+
+        // Remove the agent from the list
+        agentqueue.RemoveAt(0);
+
+        // Notify the agent that it is leaving the queue
         agent.MoveFromQueue();
 
+        // Set the agent's destination to a random location
         agent.SetRandomDestination();
+
+        // Shift the positions of the remaining agents in the queue
         Shift();
-        return agent;  
+
+        // Return the removed agent
+        return agent;
     }
 
-    // Size of queue
-    public int Size() {
+    // Get the size of the queue (number of agents in the queue)
+    public int Size()
+    {
         return agentqueue.Count;
     }
 
-    // Update is called once per frame
+    // Update method is called once per frame
     void Update()
     {
+        // Check if an agent is currently in the process of entering the queue
         if (incomingAgent == null)
-            return;
+        {
+            return; // No action required
+        }
 
-        if (incomingAgent.IsInQueue()) {
+        // Check if the incoming agent has reached its destination
+        if (incomingAgent.IsInQueue())
+        {
+            // Disable the incoming agent's navigation mesh agent
             incomingAgent.TurnOffNavMeshAgent(qPos);
+
+            // Add the incoming agent to the queue
             agentqueue.Add(incomingAgent);
+
+            // Reset the reference to the incoming agent
             incomingAgent = null;
-            Debug.Log("Queue for "+gameObject.name+" now has size "+agentqueue.Count+". Next pos: "+qPos);
-        }    
+
+            // Log the updated queue size and the next queue position
+            Debug.Log("Queue for " + gameObject.name + " now has size " + agentqueue.Count + ". Next pos: " + qPos);
+        }
     }
 }
