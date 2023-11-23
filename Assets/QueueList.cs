9bbfb75c37b50d6d8fc9faca354d9e5f7118b0b2
ArifHakimii <89134407+ArifHakimii@users.noreply.github.com>
2023-11-24 03:00:15 +0800

    Update QueueList.cs

--- Assets/QueueList.cs
+++ Assets/QueueList.cs
@@ -2,23 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu (menuName="Behavior/QueueList")]
+[CreateAssetMenu(menuName = "Behavior/QueueList")]
 public class QueueList : ScriptableObject
 {
-
+    // A list to store all the active agent queues in the game
     [System.NonSerialized]
     List<AgentQueue> queues = new List<AgentQueue>();
 
-    public AgentQueue Get(int i) {
+    // Get the agent queue at the specified index from the list
+    public AgentQueue Get(int i)
+    {
+        // Return the agent queue at the specified index
         return queues[i];
     }
 
-    public int Count() {
+    // Get the total number of agent queues in the list
+    public int Count()
+    {
+        // Return the number of elements in the queue list
         return queues.Count;
     }
 
-    public void Add(AgentQueue queue) 
+    // Add a new agent queue to the list
+    public void Add(AgentQueue queue)
     {
+        // Add the specified agent queue to the list
         queues.Add(queue);
     }
 }
