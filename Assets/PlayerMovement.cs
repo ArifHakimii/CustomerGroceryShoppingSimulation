6d6b7e8e2531c5b0523aeafe0e2b0f367a91ea49
ArifHakimii <89134407+ArifHakimii@users.noreply.github.com>
2023-11-24 02:49:24 +0800

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,76 +5,95 @@ using TMPro;
 
 public class PlayerMovementTutorial : MonoBehaviour
 {
+    // Define variables related to movement
     [Header("Movement")]
-    public float moveSpeed;
+    public float moveSpeed; // Base movement speed
+    public float groundDrag; // Ground friction
+    public float jumpForce; // Jump force
+    public float jumpCooldown; // Time between jumps
+    public float airMultiplier; // Movement speed multiplier in air
+    bool readyToJump; // Flag indicating whether the player can jump
 
-    public float groundDrag;
-
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool readyToJump;
-
+    // Define hidden variables to store different movement speeds
     [HideInInspector] public float walkSpeed;
     [HideInInspector] public float sprintSpeed;
 
+    // Define keybinds for actions
     [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode jumpKey = KeyCode.Space; // Key for jumping
 
+    // Define variables for ground check
     [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
+    public float playerHeight; // Height of the player's feet
+    public LayerMask whatIsGround; // Layer mask to identify the ground
+    bool grounded; // Flag indicating whether the player is on the ground
 
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
+    // Define reference variables for orientation and movement
+    public Transform orientation; // Transform for player's orientation
+    float horizontalInput; // Horizontal input from the player
+    float verticalInput; // Vertical input from the player
+    Vector3 moveDirection; // Direction of player's movement
+    Rigidbody rb; // Rigidbody component of the player
 
     private void Start()
     {
+        // Get the Rigidbody component of the player
         rb = GetComponent<Rigidbody>();
+
+        // Prevent the player from rotating freely
         rb.freezeRotation = true;
 
+        // Initialize the flag indicating the player can jump
         readyToJump = true;
     }
 
+    // Update method is called once per frame
     private void Update()
     {
-        // ground check
+        // Check if the player is on the ground
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
+        // Process player input
         MyInput();
+
+        // Adjust movement speed based on input
         SpeedControl();
 
-        // handle drag
+        // Apply ground drag or no drag depending on the player's state
         if (grounded)
-            rb.drag = groundDrag;
+        {
+            rb.drag = groundDrag; // Apply ground drag
+        }
         else
-            rb.drag = 0;
+        {
+            rb.drag = 0; // Remove drag when in the air
+        }
     }
 
+    // FixedUpdate method is called once per physics update
     private void FixedUpdate()
     {
+        // Move the player based on the calculated movement direction
         MovePlayer();
     }
 
+    // Handle player input for movement and jumping
     private void MyInput()
     {
+        // Get horizontal and vertical input values
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        // when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        // Check if the jump key is pressed and the player is on the ground and ready to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
+            // Set the flag indicating the player is not ready to jump
             readyToJump = false;
 
+            // Make the player jump
             Jump();
 
+            // Call the ResetJump method after the jump cooldown period
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
